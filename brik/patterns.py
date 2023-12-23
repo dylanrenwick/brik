46ee8e6341d906e14f5bb05798e5142040476080
Dylan Renwick <skidsirongenesisdev@gmail.com>
2023-12-23 00:16:13 -0500

    Fixed extra space

--- brik/patterns.py
+++ brik/patterns.py
@@ -5,5 +5,5 @@ class Pattern:
     def __init__(self, args: list[Tuple[str, DataType]]):
         self.args = args
     def __str__(self)-> str:
-        return f'<{' '.join([f'{name}: {datatype.type.name.lower()}' for (name, datatype) in self.args])}>'
+        return f'<{' '.join([f'{name}:{datatype.type.name.lower()}' for (name, datatype) in self.args])}>'
 
